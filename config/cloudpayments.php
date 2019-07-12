664d19917fbf9d16ab0d1cc33783ac08ac5a99b7
Stanislav Kabin <me@h-zone.ru>
2019-07-12 14:43:27 +0300

    Update cloudpayments.php

--- config/cloudpayments.php
+++ config/cloudpayments.php
@@ -4,5 +4,5 @@ return [
     'apiSecret' => env( CLOUDPAYMENT_SECRET, '' ),
     'publicId' => env( CLOUDPAYMENT_PUBLIC_ID, '' ),
     'apiUrl' => env( CLOUDPAYMENT_API_URL, 'https://api.cloudpayments.ru' ),
-    'cultureName' => env( CLOUDPAYMENT_CULTURE, 'en-US' ), // https://cloudpayments.ru/Docs/Api#language
+    'cultureName' => env( CLOUDPAYMENT_CULTURE, ( app()->getLocale() == 'ru' ? 'ru-RU' : 'en-US' ) ), // https://cloudpayments.ru/Docs/Api#language
 ];
